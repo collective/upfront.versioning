a00188da4ac02cb49d5b1eb272805739546345e3
hedley <hedley@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2009-11-08 13:30:22 +0000

    Add test for version incrementing

--- upfront/versioning/tests/test_checkin.py
+++ upfront/versioning/tests/test_checkin.py
@@ -40,6 +40,35 @@ class TestCheckin(VersioningTestCase):
         # Grandparent folder of checkedin must have id 'document'
         self.assertEquals(checkedin.aq_parent.aq_parent.id, 'document')
 
+    def test_second_checkin(self):
+        """Content is already in repo, ie. there exists a 
+        /repository/document/00000001."""
+
+        # Create /repository/document/00000001
+        document_folder = _createObjectByType(
+            'Folder', self.portal.repository, 'document'
+        )
+        version_folder = _createObjectByType(
+            'Folder', document_folder, '00000001'
+        )
+
+        utility = getUtility(IVersioner)
+        workspace = utility.getWorkspace(self.portal)
+        working = _createObjectByType('Document', workspace, 'working')
+        # We need to commit here so that _p_jar isn't None and move
+        # will work
+        transaction.savepoint(optimistic=True)
+        checkedin = utility.checkin(working)
+
+        # Working copy must be removed from workspace
+        self.failIf('working' in workspace.objectIds())
+
+        # Parent folder of checkedin must have id 00000002
+        self.assertEquals(checkedin.aq_parent.id, '00000002')
+
+        # Grandparent folder of checkedin must have id 'document'
+        self.assertEquals(checkedin.aq_parent.aq_parent.id, 'document')
+
 def test_suite():
     from unittest import TestSuite, makeSuite
 
