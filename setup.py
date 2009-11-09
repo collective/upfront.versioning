f8dc15c399f3a9c7e895b5f23fc5c5bdc719d596
hedley <hedley@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2009-11-09 14:24:13 +0000

    Add some metadata

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ version = '0.1'
 
 setup(name='upfront.versioning',
       version=version,
-      description="",
+      description="Versioning for objects",
       long_description=open("README.txt").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
@@ -15,9 +15,9 @@ setup(name='upfront.versioning',
         "Topic :: Software Development :: Libraries :: Python Modules",
         ],
       keywords='',
-      author='',
-      author_email='',
-      url='',
+      author='Hedley Roos',
+      author_email='hedley@upfrontsystems.co.za',
+      url='http://www.upfrontsystems.co.za',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['upfront'],
