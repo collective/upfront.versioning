aa8077135860287a4e8a8e0f0b02c0ea4a185f7f
hedley <hedley@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2009-11-15 20:51:04 +0000

    Expand viewlet

--- upfront/versioning/browser/viewlets/versions.py
+++ upfront/versioning/browser/viewlets/versions.py
@@ -1,7 +1,14 @@
+from Acquisition import aq_inner
 from zope.interface import implements
 from zope.viewlet.interfaces import IViewlet
+
 from Products.Five.browser import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from Products.CMFCore.utils import getToolByName
+from Products.CMFCore.permissions import View
+from plone.memoize.instance import memoize
+
+from upfront.versioning.interfaces import IVersionMetadata
 
 class VersionsViewlet(BrowserView):
     implements(IViewlet)
@@ -14,5 +21,24 @@ class VersionsViewlet(BrowserView):
 
     def update(self):
         pass
+
+    @memoize
+    def versions(self):
+        """Return versions info as a list of dictionaries"""
+        vc = getToolByName(self.context, 'upfront_versioning_catalog')
+        pms = getToolByName(self.context, 'portal_membership')
+        member = pms.getAuthenticatedMember()
+        li = []
+        for brain in vc.getVersionsOf(aq_inner(self.context)):
+            ob = brain.getObject()
+            di = dict(
+                state=IVersionMetadata(ob).state,
+                owner=ob.getOwner(),
+                version='xxx',
+                url=member.has_permission(View, ob) and brain.getURL() or None,
+                title=ob.title,
+            )    
+            li.append(di)
+        return li
 
     render = ViewPageTemplateFile("versions.pt")
